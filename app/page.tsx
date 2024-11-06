12d7d581dc53ca2ffe39d76ad2cba038f06f2009
hashirkamilbaig <hashirkamilbaig2003@gmail.com>
2024-11-05 23:59:16 -0800

    removed aproach and clients files and there files

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ import { navItems } from "@/data";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
-import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
@@ -21,9 +19,7 @@ const Home = () => {
         <Grid />
         <RecentProjects />
         <ExpandableCardDemo />
-        <Clients />
         <Experience />
-        <Approach />
         <Footer />
       </div>
     </main>
