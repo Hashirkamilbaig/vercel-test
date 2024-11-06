cb6d8cd1f07cfee00ecbbbf362380456bc0f3b1e
hashirkamilbaig <hashirkamilbaig2003@gmail.com>
2024-11-05 19:21:52 -0800

    fixed project section - still need to work on the picture alignment but it works perfectly on phone

--- data/index.ts
+++ data/index.ts
@@ -74,35 +74,19 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
+    title: "Software Developer Portfolio",
+    des: "Built a 3D portfolio with React, Three.js, and Tailwind CSS, featuring interactive animations and a responsive layout to showcase my skills and projects.",
+    img: "/images/p1.jpg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
+    link: "https://github.com/Hashirkamilbaig?tab=repositories",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
+    title: "Habibis Kitchen",
+    des: "A dynamic game built using advanced C# features like classes, scriptable objects, and custom scripts, alongside Unity’s prefabs, sprites, and animations to bring the game’s visuals to life.",
+    img: "/images/p22.jpg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/ui.yoom.com",
-  },
-  {
-    id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
+    link: "https://github.com/Hashirkamilbaig?tab=repositories",
   },
 ];
 
